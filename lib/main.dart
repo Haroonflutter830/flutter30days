d78dea1620f1484001e3eca318b229c616ce0528
haroonflutter <haroonuk34@gmail.com>
2025-11-04 08:47:33 -0800

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "the nation",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('day1'),
+      ),
+      body: Center(child:  Text('college terbala')),
+    ),
+  ));
+}
